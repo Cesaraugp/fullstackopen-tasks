f8e8244c650a64928b9797523e4c598438f09199
Cesar Pérez <cesaraugp01@gmail.com>
2021-04-08 07:20:58 -0400

    1.1: course information, step1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.parts[0]} {props.exercises[0]}
+      </p>
+      <p>
+        {props.parts[1]} {props.exercises[1]}
+      </p>
+      <p>
+        {props.parts[2]} {props.exercises[2]}
+      </p>
+    </>
+  );
+};
+const Total = (props) => {
+  return <p>Number of exercises {props.e1 + props.e2 + props.e3}</p>;
+};
 
 function App() {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of Component";
+  const exercises3 = 14;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course} />
+      <Content
+        parts={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
+      <Total e1={exercises1} e2={exercises2} e3={exercises3} />
     </div>
   );
 }
