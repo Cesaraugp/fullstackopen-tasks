7b7359f014e158863e4c34d18b811185bb00d51a
Cesar Pérez <cesaraugp01@gmail.com>
2021-04-09 07:02:46 -0400

    1.5: course information step5 ✔

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -32,27 +32,28 @@ const Total = (props) => {
 };
 
 function App() {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of Component",
-      exercises: 14,
-    },
-  ];
-
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of Component",
+        exercises: 14,
+      },
+    ],
+  };
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 }
