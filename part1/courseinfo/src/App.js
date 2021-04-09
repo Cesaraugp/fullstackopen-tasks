cf06db1179298f9a7889cd8a3b3c0a5ef50bb5f0
Cesar Pérez <cesaraugp01@gmail.com>
2021-04-09 06:55:40 -0400

    1.3: course information step3 ✔

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -26,20 +26,27 @@ const Total = (props) => {
 
 function App() {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of Component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of Component",
+    exercises: 14,
+  };
+
   return (
     <div>
       <Header course={course} />
       <Content
-        parts={[part1, part2, part3]}
-        exercises={[exercises1, exercises2, exercises3]}
+        parts={[part1.name, part2.name, part3.name]}
+        exercises={[part1.exercises, part2.exercises, part3.exercises]}
       />
-      <Total e1={exercises1} e2={exercises2} e3={exercises3} />
+      <Total e1={part1.exercises} e2={part2.exercises} e3={part3.exercises} />
     </div>
   );
 }
