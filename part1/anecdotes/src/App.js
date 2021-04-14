0760f3c1b4f447964793afafda658650e4840f3e
Cesar Pérez <cesaraugp01@gmail.com>
2021-04-14 07:23:09 -0400

    1.13-1.14 anecdotes step2 & step3✔

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -4,9 +4,50 @@ const getRandomNumber = () => {
   const max = Math.floor(6);
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 };
-
-const RandomQuote = ({ anecdotes, number }) => {
-  return <p>{anecdotes[number]}</p>;
+const VoteButton = ({ number, points, setPoints }) => {
+  const copy = [...points];
+  copy[number] += 1;
+  return (
+    <button
+      onClick={() => {
+        setPoints([...copy]);
+      }}
+    >
+      Vote
+    </button>
+  );
+};
+const NextButton = ({ handleClick }) => {
+  return <button onClick={handleClick}>Next Anecdote</button>;
+};
+const RandomQuote = ({ anecdotes, number, title, hasVotes = true }) => {
+  if (!hasVotes) {
+    return (
+      <>
+        <h1>{title}</h1>
+        <p>No votes were given yet</p>
+      </>
+    );
+  }
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>{anecdotes[number]}</p>
+    </>
+  );
+};
+const QuoteOfTheDay = ({ points, anecdotes }) => {
+  const isAllZero = points.every((item) => item === 0);
+  let indexOfMaxValue = 0;
+  if (!isAllZero) indexOfMaxValue = points.indexOf(Math.max(...points));
+  return (
+    <RandomQuote
+      anecdotes={anecdotes}
+      number={indexOfMaxValue}
+      title="Anecdote with most votes"
+      hasVotes={!isAllZero}
+    />
+  );
 };
 const App = () => {
   const anecdotes = [
@@ -18,18 +59,23 @@ const App = () => {
     "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
   ];
 
+  const [points, setPoints] = useState(new Uint8Array(6));
   const [selected, setSelected] = useState(0);
 
   return (
     <div>
-      <RandomQuote anecdotes={anecdotes} number={selected}></RandomQuote>
-      <button
-        onClick={() => {
+      <RandomQuote
+        anecdotes={anecdotes}
+        number={selected}
+        title="Anecdote of the day"
+      />
+      <QuoteOfTheDay points={[...points]} anecdotes={anecdotes} />
+      <VoteButton number={selected} points={points} setPoints={setPoints} />
+      <NextButton
+        handleClick={() => {
           setSelected(getRandomNumber);
         }}
-      >
-        Next Anecdote
-      </button>
+      />
     </div>
   );
 };
