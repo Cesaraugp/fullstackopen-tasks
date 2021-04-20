97ff262dbc35382f08e0301ece41ba632a54cad4
Cesar Pérez <cesaraugp01@gmail.com>
2021-04-20 07:29:59 -0400

    useEffect for fetching notes from db in json-server

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,10 +1,23 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
 import Note from "./components/Note.js";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note");
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      const data = response.data;
+      setNotes(data);
+    });
+  };
+  useEffect(hook, []);
+
+  console.log("render", notes.length, "notes");
+
   const handleNoteChange = (event) => {
     setNewNote(event.target.value);
   };
