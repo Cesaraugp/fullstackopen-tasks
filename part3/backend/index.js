4200c541d3b40ee8e66353d3c0031541add1d33f
Cesar Pérez <cesaraugp01@gmail.com>
2021-04-28 20:00:49 -0400

    🆕 post notes

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -2,6 +2,13 @@ const { response, request } = require("express");
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
 let notes = [
   {
     id: 1,
@@ -48,6 +55,26 @@ app.delete("/api/notes/:id", (request, response) => {
   response.status(204).end();
 });
 
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  res.json(note);
+});
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
