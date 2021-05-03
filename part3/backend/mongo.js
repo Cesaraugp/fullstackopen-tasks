a5f0314c0c1ba82dd7bd86bb4ee75c881cd0f94b
Cesar Pérez <cesaraugp01@gmail.com>
2021-05-03 07:31:01 -0400

    Adding MongoDB to notes backend

---
+++ part3/backend/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log(
+    "Please provide the password as an argument: node mongo.js <password>"
+  );
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://cesaraugp:${password}@cluster0.zl88z.mongodb.net/note-app?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "HTML is Easy",
+  date: new Date(),
+  important: true,
+});
+/* 
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
+ */
+Note.find({}).then((r) => {
+  r.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
