f88e5489ec8e2eaba43b7cccdaf55d59600d2d70
Cesar Pérez <cesaraugp01@gmail.com>
2021-05-17 08:43:17 -0400

    Lint Corrections of notes backend

--- part3/backend/models/note.js
+++ part3/backend/models/note.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
-const url = process.env.MONGODB_URI;
+const mongoose = require('mongoose')
+const url = process.env.MONGODB_URI
 
-console.log("connecting to ", url);
+console.log('connecting to ', url)
 
 mongoose
   .connect(url, {
@@ -10,12 +10,12 @@ mongoose
     useFindAndModify: false,
     useCreateIndex: true,
   })
-  .then((r) => {
-    console.log("🟢 connected to MongoDB");
+  .then(() => {
+    console.log('🟢 connected to MongoDB')
   })
   .catch((error) => {
-    console.log("🔴 Error Connecting to MongoDB: ", error.message);
-  });
+    console.log('🔴 Error Connecting to MongoDB: ', error.message)
+  })
 
 const noteSchema = new mongoose.Schema({
   content: {
@@ -28,13 +28,13 @@ const noteSchema = new mongoose.Schema({
     required: true,
   },
   important: Boolean,
-});
-noteSchema.set("toJSON", {
+})
+noteSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Note", noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
--- part3/backend/mongo.js
+++ part3/backend/mongo.js
@@ -1,40 +1,35 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
   console.log(
-    "Please provide the password as an argument: node mongo.js <password>"
-  );
-  process.exit(1);
+    'Please provide the password as an argument: node mongo.js <password>'
+  )
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://cesaraugp:${password}@cluster0.zl88z.mongodb.net/note-app?retryWrites=true&w=majority`;
+const url = `mongodb+srv://cesaraugp:${password}@cluster0.zl88z.mongodb.net/note-app?retryWrites=true&w=majority`
 
 mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true,
-});
+})
 
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
   important: Boolean,
-});
+})
 
-const Note = mongoose.model("Note", noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: "HTML is Easy",
-  date: new Date(),
-  important: true,
-});
 
 Note.find({}).then((r) => {
   r.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
