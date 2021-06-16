6d07d7e6adb587d9f72087afc6a6316630bf63d3
Cesar Perez <cesaraugp01@gmail.com~>
2021-06-16 09:16:13 -0400

    4.4-4.5: helper functions and unit tests, steps2,3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -3,6 +3,25 @@ const dummy=(blogs)=>{
     return 1;
 }
 
+const totalLikes=(blogs)=>{
+    
+    const likes= blogs.map((x)=>x.likes)
+    return likes.reduce((s,i)=>s+i,0);
+}
+const favoriteBlog=(blogs)=>{
+    if(blogs.length<1) return {};
+    const max = blogs.reduce(function(prev, current) {
+        return (prev.likes > current.likes) ? prev : current
+    })
+    return {
+        title: max['title'],
+        author: max['author'],
+        likes: max['likes']
+    }
+
+}
 module.exports={
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
