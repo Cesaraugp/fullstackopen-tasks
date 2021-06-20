a1e5d7eb170f5269f439c4adfc7f6c09d672110a
Cesar Perez <cesaraugp01@gmail.com~>
2021-06-19 20:17:54 -0400

    Corrections on supertest

--- part3/backend/tests/note_api.test.js
+++ part3/backend/tests/note_api.test.js
@@ -6,21 +6,23 @@ const api= supertest(app);
 
 test('Checking that the get endpoint is available',async()=>{
     await api
-    .get('api/notes')
+    .get('/api/notes/')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
 
-afterAll(() => {
-    mongoose.connection.close()
-})
-
-test('there are two notes', async () => {
-    const response = await api.get('/api/notes')
-    expect(response.body).toHaveLength(2)
+test('there are five notes', async () => {
+    const response = await api.get('/api/notes/')
+   
+    expect(response.body).toHaveLength(5)
   })
   
 test('the first note is about HTTP methods', async () => {
     const response = await api.get('/api/notes')
-    expect(response.body[0].content).toBe('HTML is easy')
+    expect(response.body[0].content).toBe('HTML is Easy')
+})
+
+
+afterAll(() => {
+    mongoose.connection.close()
 })
