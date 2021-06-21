f56203520205a2a1783c0fd9e89b4800e3fbc737
Cesar Perez <cesaraugp01@gmail.com~>
2021-06-21 19:54:08 -0400

    eliminating the try-catch

--- part3/backend/controllers/notes.js
+++ part3/backend/controllers/notes.js
@@ -8,27 +8,19 @@ notesRouter.get("/", async (request, response) => {
 
 notesRouter.get("/:id", async (request, response, next) => {
   const id = request.params.id;
-  try {
     note = await Note.findById(id);
     if (note) {
       response.json(note);
     } else {
       response.status(404).end();
-    }
-  } catch (error) {
-    next(error);
   }
 });
 
 notesRouter.delete("/:id", async (request, response, next) => {
   const id = request.params.id;
-  try {
-    await Note.findByIdAndRemove(id);
-    response.status(204).end();
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+  await Note.findByIdAndRemove(id);
+  response.status(204).end();
+   
 });
 
 notesRouter.post("/", async (req, res, next) => {
@@ -45,12 +37,9 @@ notesRouter.post("/", async (req, res, next) => {
     important: body.important || false,
     date: new Date(),
   });
-  try {
-    const savedNote = await note.save();
-    res.json(savedNote);
-  } catch (error) {
-    next(error);
-  }
+  const savedNote = await note.save();
+  res.json(savedNote);
+ 
 });
 
 notesRouter.put("/:id", async (req, res, next) => {
@@ -59,14 +48,12 @@ notesRouter.put("/:id", async (req, res, next) => {
     content: body.content,
     important: body.important,
   };
-  try {
+  
     const updatedNote = await Note.findByIdAndUpdate(req.params.id, note, {
       new: true,
-    });
+    }); 
     res.json(updatedNote);
-  } catch (error) {
-    next(error);
-  }
+
 });
 
 module.exports = notesRouter;
