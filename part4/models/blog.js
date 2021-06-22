fb8f4aa5f5e3be6c035e196d35fe77665bd1fa33
Cesar Perez <cesaraugp01@gmail.com~>
2021-06-21 20:39:07 -0400

    4.9*: Blog list tests, step2

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -6,5 +6,12 @@ const blogSchema = new mongoose.Schema({
   url: String,
   likes: Number
 })
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
 
 module.exports= mongoose.model('Blog', blogSchema);
--- part4/tests/blogs_tests.test.js
+++ part4/tests/blogs_tests.test.js
@@ -14,13 +14,17 @@ beforeEach(async()=>{
         return blogToSave.save();
     });
     await Promise.all(blogsArray);
-},8000)
+},15000)
 
 describe('Blogs content',()=>{
     test('Get the amount of blogs',async()=>{
         const response = await api.get("/api/blogs/");
         expect(response.body).toHaveLength(helper.initialBlogsList.length);
     })
+    test('Property name id',async()=>{
+        const response = await api.get("/api/blogs/");
+        expect(response.body[0].id).toBeDefined();
+    })  
 })
 
 afterAll(()=>{
