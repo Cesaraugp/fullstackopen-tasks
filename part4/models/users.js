b41ad5eb978135e679b2ae74db667bbc41af8ac3
Cesar Perez <cesaraugp01@gmail.com>
2021-06-30 08:09:09 -0400

    4.15: bloglist expansion, step3

---
+++ part4/models/users.js
@@ -0,0 +1,26 @@
+const mongoose= require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+  
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
+})
+
+module.exports= mongoose.model('User',userSchema);
+
+/*blogs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Blog'
+    }
+  ]*/
