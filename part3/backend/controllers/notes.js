8fe9b16ff2113368cabf04025d2096784f640e66
Cesar Pérez <cesaraugp01@gmail.com>
2021-07-06 20:55:16 -0400

    login in frontend

--- part3/backend/controllers/notes.js
+++ part3/backend/controllers/notes.js
@@ -1,22 +1,21 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
-const User = require('../models/users')
-const jwt= require('jsonwebtoken');
+const User = require("../models/users");
+const jwt = require("jsonwebtoken");
 
 notesRouter.get("/", async (request, response) => {
- const notes = await Note
-    .find({}).populate('user', { username: 1, name: 1 })
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
 
-  response.json(notes)
+  response.json(notes);
 });
 
 notesRouter.get("/:id", async (request, response, next) => {
   const id = request.params.id;
-    note = await Note.findById(id);
-    if (note) {
-      response.json(note);
-    } else {
-      response.status(404).end();
+  note = await Note.findById(id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
   }
 });
 
@@ -24,38 +23,32 @@ notesRouter.delete("/:id", async (request, response, next) => {
   const id = request.params.id;
   await Note.findByIdAndRemove(id);
   response.status(204).end();
-   
 });
 
-notesRouter.post('/', async (request, response, next) => {
-  const body = request.body
-<<<<<<< HEAD
+notesRouter.post("/", async (request, response, next) => {
+  const body = request.body;
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: "token missing or invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
 
-  const user = await User.findById(body.userId)
-=======
-  const token=getTokenFrom(request);
-  const decodedToken= jwt.verify(token,process.env.SECRET);
-  if (!token || !decodedToken.id) {    
-      return response.status(401).json({ error: 'token missing or invalid' })
-  }  
-  const user = await User.findById(decodedToken.id)
-  
   //const user = await User.findById(body.userId)
->>>>>>> 17094f658297700c358ac86cee76ab2261f99f5e
 
   const note = new Note({
     content: body.content,
     important: body.important === undefined ? false : body.important,
     date: new Date(),
-    user: user._id
-  })
+    user: user._id,
+  });
 
-  const savedNote = await note.save()
-  user.notes = user.notes.concat(savedNote._id)
-  await user.save()
-  
-  response.json(savedNote)
-})
+  const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
+
+  response.json(savedNote);
+});
 
 notesRouter.put("/:id", async (req, res, next) => {
   const body = req.body;
@@ -63,24 +56,19 @@ notesRouter.put("/:id", async (req, res, next) => {
     content: body.content,
     important: body.important,
   };
-  
-    const updatedNote = await Note.findByIdAndUpdate(req.params.id, note, {
-      new: true,
-    }); 
-    res.json(updatedNote);
 
+  const updatedNote = await Note.findByIdAndUpdate(req.params.id, note, {
+    new: true,
+  });
+  res.json(updatedNote);
 });
 
-<<<<<<< HEAD
-=======
-const getTokenFrom= request=>{
-  const authorization= request.get('authorization');
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) { 
-    return authorization.substring(7)  
-  }  
-  return null
-}
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
-
->>>>>>> 17094f658297700c358ac86cee76ab2261f99f5e
 module.exports = notesRouter;
---
+++ part5/src/App.js
@@ -0,0 +1,159 @@
+import React, { useState, useEffect } from "react";
+import Note from "./components/Note";
+import Notification from "./components/Notification";
+import Footer from "./components/Footer";
+import noteService from "./services/notes";
+import loginService from "./services/login";
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [user, setUser] = useState(null);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedNoteappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      noteService.setToken(user.token);
+    }
+  }, []);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+    };
+
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
+  };
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+      });
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user));
+      noteService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong Credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={handleNoteChange} />
+      <button type="submit">save</button>
+    </form>
+  );
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+
+      <h2>Login</h2>
+      {user == null ? (
+        loginForm()
+      ) : (
+        <div>
+          <p>{user.name} logged-in</p>
+          {noteForm()}
+        </div>
+      )}
+
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
