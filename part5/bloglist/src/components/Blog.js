500ea8cdc99a175d5b3d098e2288db48352dafd3
Cesar Pérez <cesaraugp01@gmail.com>
2021-08-07 20:42:48 -0400

    5.7* Blog list frontend, step7

--- part5/bloglist/src/components/Blog.js
+++ part5/bloglist/src/components/Blog.js
@@ -1,8 +1,33 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
 
-export default Blog
+  const [allInfoVisible, setAllInfoVisible] = useState(false);
+  const toggleVisible = () => setAllInfoVisible(!allInfoVisible);
+  return (
+    <div style={blogStyle}>
+      {blog.title} - {blog.author}
+      <button onClick={toggleVisible}>
+        {allInfoVisible ? "Hide" : "View"}
+      </button>
+      {allInfoVisible && (
+        <div>
+          <p>{blog.url} </p>
+          <p>
+            Likes:
+            {blog.likes}
+            <button>like</button>{" "}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
