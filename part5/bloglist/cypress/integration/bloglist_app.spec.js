a63400b9aac64b1c7c70716a165a60465da5c937
cesaraugp <cesaraugp01@gmail.com>
2021-08-13 19:30:14 -0400

    5.18: bloglist end to end testing, step2

--- part5/bloglist/cypress/integration/bloglist_app.spec.js
+++ part5/bloglist/cypress/integration/bloglist_app.spec.js
@@ -1,10 +1,36 @@
 describe('Blog app', function() {
     beforeEach(function() {
       cy.request('POST', 'http://localhost:3003/api/testing/reset')
+      const user = {
+        name: 'cesar perez',
+        username: 'cesar',
+        password: 'password'
+      }
+      cy.request('POST', 'http://localhost:3003/api/users/', user)
+
       cy.visit('http://localhost:3000')
     })
   
     it('Login form is shown', function() {
       cy.get('#login-form')
     })
+
+    describe('login',function(){
+        it('succeeds with correct credentials', function() {
+            cy.get('#username').type('cesar')
+            cy.get('#password').type('password')
+            cy.get('#login-button').click()
+            cy.contains('Welcome! cesar perez, you are logged in')
+          })
+          it('fails with wrong credentials', function() {
+            cy.get('#username').type('pedro')
+            cy.get('#password').type('cacahuate')
+            cy.get('#login-button').click()
+            cy.get('.error').should('contain','error: Wrong Credentials')
+            .should('have.css', 'color', 'rgb(255, 0, 0)')
+            .should('have.css', 'border-style', 'solid')
+            //cy.contains('Welcome! cesar perez, you are logged in')
+          })
+    })
+
   })
--- part5/bloglist/src/App.js
+++ part5/bloglist/src/App.js
@@ -24,9 +24,11 @@ const App = () => {
   }, [])
 
   useEffect(() => {
-    blogService
-      .getAll()
-      .then((blogs) => setBlogs(blogs.sort((a, b) => b.likes - a.likes)))
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      blogService
+        .getAll()
+        .then((blogs) => setBlogs(blogs.sort((a, b) => b.likes - a.likes)))}
   }, [])
 
   const loginForm = () => (
@@ -35,6 +37,7 @@ const App = () => {
         username
         <input
           type="text"
+          id="username"
           value={username}
           name="Username"
           onChange={({ target }) => setUsername(target.value)}
@@ -44,12 +47,13 @@ const App = () => {
         password
         <input
           type="password"
+          id="password"
           value={password}
           name="Password"
           onChange={({ target }) => setPassword(target.value)}
         />
       </div>
-      <button type="submit">login</button>
+      <button id='login-button' type="submit">login</button>
     </form>
   )
   const blogForm = () => (
